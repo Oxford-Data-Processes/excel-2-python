a15bb14ce56b9b4af42e37cdf979a6651e08878c
Chris Little <chris.little@sonnedix.com>
2024-04-05 18:40:13 +0100

    Series Extractor checks

--- src/series_extraction/excel_compatibility_checker.py
+++ src/series_extraction/excel_compatibility_checker.py
@@ -1,5 +1,5 @@
-from objects import ExcelFile, Worksheet, Table
-from typing import Dict
+from objects import ExcelFile, Worksheet, Table, HeaderLocation
+from typing import Dict, List
 
 
 class ExcelCompatibilityChecker:
@@ -7,8 +7,33 @@ class ExcelCompatibilityChecker:
     def check_file(
         excel_raw: ExcelFile,
         excel_reduced: ExcelFile,
-        extracted_tables: Dict[Worksheet, Table],
+        extracted_tables: Dict[Worksheet, List[Table]],
     ) -> bool:
+        for worksheet, tables in extracted_tables.items():
+            for table in tables:
+                raw_ws = excel_raw.workbook_with_values[worksheet.sheet_name]
+                reduced_ws = excel_reduced.workbook_with_values[worksheet.sheet_name]
 
-        is_compatible = True
-        return is_compatible
+                if table.header_location == HeaderLocation.TOP:
+                    raw_headers = [
+                        cell.value for cell in raw_ws[table.range.start_cell.row]
+                    ]
+                    reduced_headers = [
+                        cell.value for cell in reduced_ws[table.range.start_cell.row]
+                    ]
+                else:
+                    raw_headers = [
+                        raw_ws[cell.coordinate].value
+                        for cell in raw_ws[table.range.start_cell.column]
+                    ]
+                    reduced_headers = [
+                        reduced_ws[cell.coordinate].value
+                        for cell in reduced_ws[table.range.start_cell.column]
+                    ]
+
+                if raw_headers != reduced_headers:
+                    print(raw_headers)
+                    print(reduced_headers)
+                    return False
+
+        return True

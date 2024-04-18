f972dfada84b3d9794c9b78ce5b3236a576f5fe6
Chris Little <chris.little@sonnedix.com>
2024-04-18 03:15:18 +0100

    Checkpoint

--- src/pipeline_building/series_dependencies_builder.py
+++ src/pipeline_building/series_dependencies_builder.py
@@ -1,7 +1,25 @@
 import xlcalculator
+import ast
+from ast_transformation.formula_generator import SeriesIdLoader
 
 
 class SeriesDependenciesBuilder:
+
+    @staticmethod
+    def get_series_ids(formula_1_ast_series):
+
+        series_range_strings = SeriesDependenciesBuilder.extract_range_values(
+            formula_1_ast_series
+        )
+
+        series_ids = []
+        for series_range_string in series_range_strings:
+            series_id_strings = ast.literal_eval(series_range_string)[0]
+            for series_id_string in series_id_strings:
+                series_id = SeriesIdLoader.load_series_id_from_string(series_id_string)
+                series_ids.append(series_id)
+        return series_ids
+
     @staticmethod
     def extract_range_values(ast):
         """Static method to extract range values from an AST."""

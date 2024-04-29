d8c28352e26c771b1a813f978424e0a9461b1658
Chris Little <chris.little@sonnedix.com>
2024-04-29 14:18:41 +0100

    Take dataclasses back out

--- src/series_extraction/series_extractor.py
+++ src/series_extraction/series_extractor.py
@@ -4,6 +4,9 @@ from objects import (
     Worksheet,
     Table,
     Cell,
+    Series,
+    SeriesId,
+    SeriesDataType,
 )
 
 from typing import Dict, List, Union
@@ -11,43 +14,43 @@ from enum import Enum
 from dataclasses import dataclass
 
 
-class SeriesDataType(Enum):
-    INT = "int"
-    STR = "str"
-    FLOAT = "float"
-    BOOL = "bool"
-    TIME = "time"
+# class SeriesDataType(Enum):
+#     INT = "int"
+#     STR = "str"
+#     FLOAT = "float"
+#     BOOL = "bool"
+#     TIME = "time"
 
 
-@dataclass(frozen=True)
-class SeriesId:
-    sheet_name: str
-    series_header: str
-    series_header_cell_row: int
-    series_header_cell_column: int
+# @dataclass(frozen=True)
+# class SeriesId:
+#     sheet_name: str
+#     series_header: str
+#     series_header_cell_row: int
+#     series_header_cell_column: int
 
-    def __str__(self):
-        return "|".join(
-            [
-                self.sheet_name,
-                self.series_header,
-                str(self.series_header_cell_row),
-                str(self.series_header_cell_column),
-            ]
-        )
+#     def __str__(self):
+#         return "|".join(
+#             [
+#                 self.sheet_name,
+#                 self.series_header,
+#                 str(self.series_header_cell_row),
+#                 str(self.series_header_cell_column),
+#             ]
+#         )
 
 
-@dataclass
-class Series:
-    series_id: SeriesId
-    worksheet: Worksheet
-    series_header: str
-    formulas: List[str]
-    values: List[Union[int, str, float, bool]]
-    header_location: HeaderLocation
-    series_starting_cell: Cell
-    series_length: int
-    series_data_type: SeriesDataType
+# @dataclass
+# class Series:
+#     series_id: SeriesId
+#     worksheet: Worksheet
+#     series_header: str
+#     formulas: List[str]
+#     values: List[Union[int, str, float, bool]]
+#     header_location: HeaderLocation
+#     series_starting_cell: Cell
+#     series_length: int
+#     series_data_type: SeriesDataType
 
 
 class SeriesExtractor:

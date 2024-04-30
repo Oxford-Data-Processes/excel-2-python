a4dcc96249e0e5bc4bb4f090a7fd8379a27dfcb2
Chris Little <chris.little@sonnedix.com>
2024-04-30 10:44:20 +0100

    Add type hints to series implementer

--- src/ast_building/series_implementer.py
+++ src/ast_building/series_implementer.py
@@ -35,6 +35,37 @@ class SeriesImplementer:
         ]
         return cells
 
+    def create_cell(self, column: int, row: int, sheet_name: str) -> Cell:
+        return Cell(
+            column=column,
+            row=row,
+            sheet_name=sheet_name,
+            coordinate=None,
+            value=None,
+            value_type=None,
+        )
+
+    def get_series_list(
+        self, cells_in_range: list[Cell], worksheet: Worksheet
+    ) -> list[Series]:
+        return [
+            self.accessor.get_series_from_cell(worksheet, cell)
+            for cell in cells_in_range
+        ]
+
+    def filter_series_list(self, series_list: list[Series]) -> list[Series]:
+        return [series for series in series_list if series is not None]
+
+    def create_series_range(
+        self, filtered_series_list: list[Series], is_column_range: bool
+    ) -> SeriesRange:
+        return SeriesRange(
+            series=[item[1] for item in filtered_series_list],
+            start_index=filtered_series_list[0][0],
+            end_index=filtered_series_list[-1][0],
+            is_column_range=is_column_range,
+        )
+
     def get_series_range_from_cell_range(
         self, sheet_name: str, cell_range: str
     ) -> SeriesRange:
@@ -48,40 +79,14 @@ class SeriesImplementer:
             is_column_range,
         ) = ExcelUtils.get_coordinates_from_range(cell_range)
 
-        cell_start = Cell(
-            column=cell_start_column,
-            row=cell_start_row,
-            sheet_name=sheet_name,
-            coordinate=None,
-            value=None,
-            value_type=None,
-        )
-        cell_end = Cell(
-            column=cell_end_column,
-            row=cell_end_row,
-            sheet_name=sheet_name,
-            coordinate=None,
-            value=None,
-            value_type=None,
-        )
+        cell_start = self.create_cell(cell_start_column, cell_start_row, sheet_name)
+        cell_end = self.create_cell(cell_end_column, cell_end_row, sheet_name)
 
-        cells_in_range = SeriesImplementer.get_cells_between(cell_start, cell_end)
-
+        cells_in_range = self.get_cells_between(cell_start, cell_end)
         worksheet = Worksheet(sheet_name=sheet_name)
-
-        series_list = [
-            self.accessor.get_series_from_cell(worksheet, cell)
-            for cell in cells_in_range
-        ]
-
-        filtered_series_list = [series for series in series_list if series is not None]
-
-        series_range = SeriesRange(
-            series=[item[1] for item in filtered_series_list],
-            start_index=filtered_series_list[0][0],
-            end_index=filtered_series_list[-1][0],
-            is_column_range=is_column_range,
-        )
+        series_list = self.get_series_list(cells_in_range, worksheet)
+        filtered_series_list = self.filter_series_list(series_list)
+        series_range = self.create_series_range(filtered_series_list, is_column_range)
 
         return series_range
 
@@ -116,7 +121,9 @@ class SeriesImplementer:
             )
         )
 
-    def update_ast(self, ast):
+    def update_ast(
+        self, ast: xlcalculator.ast_nodes.ASTNode
+    ) -> xlcalculator.ast_nodes.ASTNode:
         if isinstance(ast, xlcalculator.ast_nodes.RangeNode):
             return self.replace_range_node(ast)
         elif isinstance(ast, xlcalculator.ast_nodes.FunctionNode):
@@ -125,7 +132,9 @@ class SeriesImplementer:
             return self.replace_operator_node(ast)
         return ast
 
-    def replace_range_node(self, node):
+    def replace_range_node(
+        self, node: xlcalculator.ast_nodes.RangeNode
+    ) -> xlcalculator.ast_nodes.RangeNode:
         if "!" in node.tvalue:
             sheet_name, cell_range = ExcelUtils.extract_cell_ranges_from_string(
                 node.tvalue
@@ -146,13 +155,17 @@ class SeriesImplementer:
             )
         )
 
-    def replace_function_node(self, node):
+    def replace_function_node(
+        self, node: xlcalculator.ast_nodes.FunctionNode
+    ) -> xlcalculator.ast_nodes.FunctionNode:
         modified_args = [self.update_ast(arg) for arg in node.args]
         modified_function_node = xlcalculator.ast_nodes.FunctionNode(node.token)
         modified_function_node.args = modified_args
         return modified_function_node
 
-    def replace_operator_node(self, node):
+    def replace_operator_node(
+        self, node: xlcalculator.ast_nodes.OperatorNode
+    ) -> xlcalculator.ast_nodes.OperatorNode:
         modified_left = self.update_ast(node.left) if node.left else None
         modified_right = self.update_ast(node.right) if node.right else None
         modified_operator_node = xlcalculator.ast_nodes.OperatorNode(node.token)
